3394301c1d012b5a21556729278abf85d7bc9ad6
kim-mini <72716397+kim-mini@users.noreply.github.com>
2020-12-18 13:45:36 +0900

    '1218'

---
+++ data_set_custom/rename.py
@@ -0,0 +1,28 @@
+
+cnt = 1
+path = '/home/mini/study/Yolo_mark/x64/Release/data/img4'
+fileList = os.listdir(path)
+ext = '.jpg'
+ClassName = 'ramen'
+
+
+for filename in fileList:
+    if filename == 'Untitled.ipynb':
+        break
+
+
+    if filename.endswith(ext):
+
+        srcpath = os.path.join(path, filename)
+        if cnt < 10:
+            dstname = ClassName+'_00{}'.format(cnt)+ext
+        elif cnt < 100:
+            dstname = ClassName+'_0{}'.format(cnt)+ext
+        elif cnt < 1000:
+            dstname = ClassName + '_{}'.format(cnt) + ext
+
+        dstpath = os.path.join(path, dstname)
+
+
+        os.rename(srcpath, dstpath)
+        cnt += 1

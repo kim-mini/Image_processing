0f62f0d26fa1970c17e77729e2ae18ac66052f5d
kim-mini <72716397+kim-mini@users.noreply.github.com>
2020-12-21 15:23:30 +0900

    '1221'

--- data_set_custom/resize.py
+++ data_set_custom/resize.py
@@ -1,14 +1,17 @@
 import os
 import cv2
 
-listTmp =os.listdir( "/home/ubuntu/Downloads/img" )
-basepath = "/home/ubuntu/Downloads/img"
+# 원본파일이 있는 경로
+path = "/home/ubuntu/Downloads/data/one"
+listTmp =os.listdir(path)
+# 저장할 경로
+Savepath = "/home/ubuntu/Downloads/convert"
 
 for filename in listTmp:
-    imgpath = basepath + "/" + filename
+    imgpath = path + "/" + filename
 
     img = cv2.imread(imgpath, cv2.IMREAD_COLOR )
 
     dst = cv2.resize(img, dsize=(640, 480), interpolation=cv2.INTER_AREA)
-
-    cv2.imwrite( imgpath, dst )
+    Savefile= os.path.join(Savepath,filename)
+    cv2.imwrite( Savefile, dst )
